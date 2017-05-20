eaa84605a70233a104b665628ff37a2e488a985f
diegoferques <diegoferques@gmail.com>
2017-05-20 18:50:50 -0300

    Adicionado mais alguns atributos na entidade Customer Criado o Controller CustomerController.java com um CRUD básico, faltando ainda implementar os métodos

---
+++ src/main/java/com/cosmeticos/controller/CustomerController.java
@@ -0,0 +1,58 @@
+package com.cosmeticos.controller;
+
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+public class CustomerController {
+
+    @RequestMapping(path = "/customers", method = RequestMethod.GET)
+    @ResponseBody
+    String getCustomers() {
+        return "Exibição de todos os Customers cadastrados";
+    }
+
+    @RequestMapping(path = "/customers", method = RequestMethod.POST)
+    @ResponseBody
+    public String setCustomer(HttpServletResponse response,
+                              @RequestParam(required = false) String name) {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+
+        String customerName;
+
+        if (StringUtils.isEmpty(name)) {
+            customerName = "de nome indefinido!";
+        } else {
+            customerName = "de nome " + name;
+        }
+
+        return "Criando novo Customer " + customerName;
+    }
+
+    @RequestMapping(path = "/customers/{idCustomer}", method = RequestMethod.GET)
+    @ResponseBody
+    String getCustomer(@PathVariable String idCustomer) {
+
+        return "Exibição do Customer de ID " + idCustomer;
+    }
+
+    @RequestMapping(path = "/customers/{idCustomer}", method = RequestMethod.PUT)
+    @ResponseBody
+    String updateCustomer(@PathVariable String idCustomer) {
+
+        return "Atualizando o Customer de ID " + idCustomer;
+    }
+
+    @RequestMapping(path = "/customers/{idCustomer}", method = RequestMethod.DELETE)
+    @ResponseBody
+    String deleteCustomer(@PathVariable String idCustomer) {
+
+        return "Customer de ID " + idCustomer + " Deletado com sucesso!";
+    }
+
+
+
+}
