56981b81ce72c64eae238e891f8bedfdbea7f83e
diegoferques <diegoferques@gmail.com>
2017-06-08 08:24:30 -0300

    Inicio dos testes dos endPoints de CustomerController com RestTemplate

--- src/test/java/com/cosmeticos/controller/CustomerControllerTests.java
+++ src/test/java/com/cosmeticos/controller/CustomerControllerTests.java
@@ -3,13 +3,16 @@ package com.cosmeticos.controller;
 import com.cosmeticos.Application;
 import com.cosmeticos.commons.CustomerRequestBody;
 import com.cosmeticos.commons.CustomerResponseBody;
-import com.cosmeticos.commons.ScheduleRequestBody;
 import com.cosmeticos.commons.ScheduleResponseBody;
 import com.cosmeticos.model.Address;
 import com.cosmeticos.model.Customer;
 import com.cosmeticos.model.User;
+import com.cosmeticos.repository.AddressRepository;
+import com.cosmeticos.repository.CustomerRepository;
+import com.cosmeticos.repository.UserRepository;
 import com.cosmeticos.service.CustomerService;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
@@ -27,10 +30,11 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 
 
 @RunWith(SpringRunner.class)
@@ -40,14 +44,104 @@ public class CustomerControllerTests {
 	@Autowired
 	private TestRestTemplate restTemplate;
 
+	@Autowired
+	private CustomerRepository customerRepository;
+
+	@Autowired
+	private AddressRepository addressRepository;
+
+	@Autowired
+	private UserRepository userRepository;
+
 	@MockBean
 	private CustomerService service;
+
+	/**
+	 * Inicializa o H2 com dados iniciais.
+	 */
+	@Before
+	public void setupTests() throws ParseException {
+
+		//TODO: pesquisar como gravar apenas dia mes e ano.
+		//c1.setBirthDate(Timestamp.valueOf(LocalDateTime.of(1980, 01, 20, 0, 0, 0)));
+		//Date birthDate1 = new SimpleDateFormat("yyyy-MM-dd").parse("1980-01-20");
+
+		Customer c1 = new Customer();
+		c1.setBirthDate(Timestamp.valueOf(LocalDateTime.MAX.of(1980, 01, 20, 0, 0, 0)));
+		c1.setCellPhone("(21) 98877-6655");
+		c1.setCpf("098.765.432-10");
+		c1.setDateRegister(Calendar.getInstance().getTime());
+		c1.setGenre('M');
+		c1.setNameCustomer("João da Silva");
+		//c1.setServiceRequestCollection(null);
+		c1.setStatus(Customer.Status.ACTIVE.ordinal());
+		c1.setIdAddress(this.createFakeAddress(c1));
+		c1.setIdLogin(this.createFakeLogin(c1));
+
+		Date birthDate2 = new SimpleDateFormat("yyyy-MM-dd").parse("1981-01-20");
+		Customer c2 = new Customer();
+		c2.setBirthDate(birthDate2);
+		c2.setCellPhone("(21) 98807-2756");
+		c2.setCpf("098.330.987-62");
+		c2.setDateRegister(Calendar.getInstance().getTime());
+		c2.setGenre('M');
+		c2.setNameCustomer("Diego Fernandes");
+		//c2.setServiceRequestCollection(null);
+		c2.setStatus(Customer.Status.ACTIVE.ordinal());
+		c2.setIdAddress(this.createFakeAddress(c2));
+		c2.setIdLogin(this.createFakeLogin(c2));
+
+		Date birthDate3 = new SimpleDateFormat("yyyy-MM-dd").parse("1982-01-20");
+		Customer c3 = new Customer();
+		c3.setBirthDate(birthDate3);
+		c3.setCellPhone("(21) 99988-7766");
+		c3.setCpf("831.846.135-15");
+		c3.setDateRegister(Calendar.getInstance().getTime());
+		c3.setGenre('F');
+		c3.setNameCustomer("Maria das Dores");
+		//c3.setServiceRequestCollection(null);
+		c3.setStatus(Customer.Status.ACTIVE.ordinal());
+		c3.setIdAddress(this.createFakeAddress(c3));
+		c3.setIdLogin(this.createFakeLogin(c3));
+
+		Date birthDate4 = new SimpleDateFormat("yyyy-MM-dd").parse("1983-01-20");
+		Customer c4 = new Customer();
+		c4.setBirthDate(birthDate4);
+		c4.setCellPhone("(21) 99887-7665");
+		c4.setCpf("816.810.695-68");
+		c4.setDateRegister(Calendar.getInstance().getTime());
+		c4.setGenre('F');
+		c4.setNameCustomer("Fernanda Cavalcante");
+		//c4.setServiceRequestCollection(null);
+		c4.setStatus(Customer.Status.INACTIVE.ordinal());
+		c4.setIdAddress(this.createFakeAddress(c4));
+		c4.setIdLogin(this.createFakeLogin(c4));
+
+		Date birthDate5 = new SimpleDateFormat("yyyy-MM-dd").parse("1984-01-20");
+		Customer c5 = new Customer();
+		c5.setBirthDate(birthDate5);
+		c5.setCellPhone("(21) 97766-5544");
+		c5.setCpf("541.913.254-81");
+		c5.setDateRegister(Calendar.getInstance().getTime());
+		c5.setGenre('M');
+		c5.setNameCustomer("José das Couves");
+		//c5.setServiceRequestCollection(null);
+		c5.setStatus(Customer.Status.ACTIVE.ordinal());
+		c5.setIdAddress(this.createFakeAddress(c5));
+		c5.setIdLogin(this.createFakeLogin(c5));
+
+		customerRepository.save(c1);
+		customerRepository.save(c2);
+		customerRepository.save(c3);
+		customerRepository.save(c4);
+		customerRepository.save(c5);
+	}
 
 	@Test
 	public void testCreateOK() throws IOException {
 
 
-		String content = new String(Files.readAllBytes(Paths.get("C:\\dev\\_freelas\\Deivison\\projetos\\cosmeticos\\src\\test\\resources\\custumerPostRequest.json")));
+		String content = new String(Files.readAllBytes(Paths.get("C:\\xampp\\htdocs\\cosmeticos\\src\\test\\resources\\custumerPostRequest.json")));
 
 		Customer customer = createFakeCustomer();
 		Address addres = createFakeAddress(customer);
@@ -74,19 +168,12 @@ public class CustomerControllerTests {
 
 	@Test
 	public void testCreateError500() throws IOException {
-
+		/**/
 		Mockito.when(
 				service.create(Mockito.anyObject())
 		).thenThrow(new RuntimeException());
 
-		Customer customer = createFakeCustomer();
-		Address addres = createFakeAddress(customer);
-		User user = createFakeLogin(customer);
-
-		CustomerRequestBody requestBody = new CustomerRequestBody();
-		requestBody.setAddress(addres);
-		requestBody.setUser(user);
-		requestBody.setCustomer(customer);
+		CustomerRequestBody requestBody = createFakeRequestBody();
 
 		final ResponseEntity<CustomerResponseBody> exchange = //
 				restTemplate.exchange( //
@@ -99,23 +186,50 @@ public class CustomerControllerTests {
 		Assert.assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, exchange.getStatusCode());
 	}
 
+	// TODO - terminar a implementação
+	@Test
+	public void testFindById() throws ParseException {
 
+		CustomerRequestBody requestBody = createFakeRequestBody();
 
+		final ResponseEntity<CustomerResponseBody> exchange = //
+				restTemplate.exchange( //
+						"/customers/1", //
+						HttpMethod.GET, //
+						new HttpEntity(requestBody), // Body
+						CustomerResponseBody.class);
 
-	private User createFakeLogin(Customer c) {
+		Assert.assertNotNull(exchange);
+		Assert.assertEquals(HttpStatus.OK, exchange.getStatusCode());
+
+	}
+
+	private CustomerRequestBody createFakeRequestBody() {
+		Customer customer = createFakeCustomer();
+		Address address = createFakeAddress(customer);
+		User user = createFakeLogin(customer);
+
+		CustomerRequestBody requestBody = new CustomerRequestBody();
+		requestBody.setAddress(address);
+		requestBody.setUser(user);
+		requestBody.setCustomer(customer);
+
+		return requestBody;
+	}
+
+	public User createFakeLogin(Customer c) {
 		User u = new User();
 		u.setEmail("diego@bol.com");
-		u.setIdLogin(1234L);
+		//u.setIdLogin(1234L);
 		u.setPassword("123qwe");
 		u.setSourceApp("google+");
 		u.setUsername("diegoferques");
-
-		u.setCustomerCollection(new ArrayList<>());
-		u.getCustomerCollection().add(c);
+		//u.getCustomerCollection().add(c);
+		userRepository.save(u);
 		return u;
 	}
 
-	private Address createFakeAddress(Customer customer) {
+	public Address createFakeAddress(Customer customer) {
 		Address a = new Address();
 		a.setAddress("Rua Perlita");
 		a.setCep("0000000");
@@ -123,24 +237,28 @@ public class CustomerControllerTests {
 		a.setCountry("BRA");
 		a.setNeighborhood("Austin");
 		a.setState("RJ");
-
-
-		a.setCustomerCollection(new ArrayList<>());
-		a.getCustomerCollection().add(customer);
+		//a.getCustomerCollection().add(customer);
+		addressRepository.save(a);
 		return a;
 	}
 
+	public Customer createFakeCustomer() {
+		Customer c1 = new Customer();
+		c1.setBirthDate(Timestamp.valueOf(LocalDateTime.MAX.of(1980, 01, 20, 0, 0, 0)));
+		c1.setCellPhone("(21) 98877-6655");
+		c1.setCpf("098.765.432-10");
+		c1.setDateRegister(Calendar.getInstance().getTime());
+		c1.setGenre('M');
+		c1.setNameCustomer("João da Silva");
+		//c1.setServiceRequestCollection(null);
+		c1.setStatus(Customer.Status.ACTIVE.ordinal());
+		c1.setIdAddress(this.createFakeAddress(c1));
+		c1.setIdLogin(this.createFakeLogin(c1));
 
-	private Customer createFakeCustomer() {
-		Customer c4 = new Customer();
-		c4.setBirthDate(Timestamp.valueOf(LocalDateTime.of(1991, 10, 21, 0, 0, 0)));
-		c4.setCellPhone("(21) 99887-7665");
-		c4.setCpf("816.810.695-68");
-		c4.setDateRegister(Calendar.getInstance().getTime());
-		c4.setGenre('F');
-		c4.setNameCustomer("Fernanda Cavalcante");
-		c4.setServiceRequestCollection(null);
-		c4.setStatus(Customer.Status.INACTIVE.ordinal());
-		return c4;
+		customerRepository.save(c1);
+
+		return c1;
+
 	}
+
 }
