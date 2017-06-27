f0f239fd4c5b8bdb76d508013ce109187f42e39b
diegoferques <diegoferques@gmail.com>
2017-06-27 00:41:57 -0300

    OrderPreLoadConfiguration.java estava marcado como modificado

--- src/main/java/com/cosmeticos/config/OrderPreLoadConfiguration.java
+++ src/main/java/com/cosmeticos/config/OrderPreLoadConfiguration.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 /**
  * Created by matto on 24/06/2017.
  */
+//VAMOS ENVIAR!
 
 @DependsOn({"schedulePreLoadConfiguration", "professionalPreLoadConfiguration", "customerPreLoadConfiguration"})
 @Configuration
