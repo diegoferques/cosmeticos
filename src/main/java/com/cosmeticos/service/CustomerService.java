29ef5e241b88ea1619e72e40d99c28cab4dfd2cd
Garry <garydias@gmail.com>
2017-06-28 02:48:03 -0300

    pequenas alteracoes que nao vao pro repo remoto

--- src/main/java/com/cosmeticos/service/CustomerService.java
+++ src/main/java/com/cosmeticos/service/CustomerService.java
@@ -55,7 +55,7 @@ public class CustomerService {
         c.setStatus(Customer.Status.ACTIVE.ordinal());
 
         c.setIdAddress(addressService.createFromCustomer(request));
-        //c.setIdLogin(userService.createFromCustomer(request));
+        c.setIdLogin(request.getCustomer().getIdLogin());
 
         return repository.save(c);
     }
