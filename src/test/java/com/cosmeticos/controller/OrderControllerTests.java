460d29da0cd515b5edbd4da280d08057fc49eb57
Garry <garydias@gmail.com>
2017-07-06 12:18:33 -0300

    Metodo adicionando duas orders

--- src/test/java/com/cosmeticos/controller/OrderControllerTests.java
+++ src/test/java/com/cosmeticos/controller/OrderControllerTests.java
@@ -6,6 +6,7 @@ import com.cosmeticos.commons.OrderResponseBody;
 import com.cosmeticos.model.*;
 import com.cosmeticos.repository.*;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,6 +44,14 @@ public class OrderControllerTests {
 
     @Autowired
     private OrderRepository orderRepository;
+
+    @Before
+    public void setup()
+    {
+        Service service = new Service();
+        service.setCategory("PEDICURE");
+        serviceRepository.save(service);
+    }
 
     //TESTANDO O RETORNO DE ORDER PELO ID
     @Test
@@ -125,12 +134,11 @@ public class OrderControllerTests {
           */
         Customer c1 = CustomerControllerTests.createFakeCustomer();
         Professional professional = ProfessionalControllerTests.createFakeProfessional();
-        Service service = new Service();
-        service.setCategory("PEDICURE");
 
         customerRepository.save(c1);
         professionalRepository.save(professional);
-        serviceRepository.save(service);
+
+        Service service = serviceRepository.findByCategory("PEDICURE");
 
         ProfessionalServices ps1 = new ProfessionalServices(professional, service);
 
@@ -205,4 +213,10 @@ public class OrderControllerTests {
 
     }
 
+
+    @Test public void testaddwallet() throws URISyntaxException {
+
+        createScheduledOrderOk();
+        createScheduledOrderOk();
+    }
 }
