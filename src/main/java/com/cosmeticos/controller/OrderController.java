d0608826da0186c386834a53859fa57b15213868
Garry <garydias@gmail.com>
2017-07-16 16:35:32 -0300

    Inclusao de jsonview para outros atributos retornaveis para GET /orders

--- src/main/java/com/cosmeticos/controller/OrderController.java
+++ src/main/java/com/cosmeticos/controller/OrderController.java
@@ -159,7 +159,7 @@ public class OrderController {
 
     }
 
-    @JsonView(ResponseJsonView.OrderControllerUpdate.class)
+    @JsonView(ResponseJsonView.OrderControllerFindBy.class)
     @RequestMapping(path = "/orders", method = RequestMethod.GET)
     public HttpEntity<OrderResponseBody> findBy(@ModelAttribute Order bindableQueryObject) {
 
