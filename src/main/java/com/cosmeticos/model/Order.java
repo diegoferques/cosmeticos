9255c0acf27b1353a9925c17b723674e19df6341
KillerJV <killerhomage@gmail.com>
2017-07-20 20:35:53 -0300

    RNF48 com pequena alteração no Order

--- src/main/java/com/cosmeticos/model/Order.java
+++ src/main/java/com/cosmeticos/model/Order.java
@@ -22,7 +22,7 @@ import java.util.Date;
 public class Order implements Serializable {
 
     public enum Status {
-        CREATED, ABORTED, EXECUTED, FINISHED_BY_PROFESSIONAL, FINISHED_BY_CUSTOMER_AUTO, FINISHED_BY_CUSTOMER, SCHEDULED;
+        OPEN, CANCELLED, EXECUTED, SEMI_CLOSED, AUTO_CLOSED, CLOSED, SCHEDULED, INPROGRESS, ACCEPTED
     }
 
     private static final long serialVersionUID = 1L;
