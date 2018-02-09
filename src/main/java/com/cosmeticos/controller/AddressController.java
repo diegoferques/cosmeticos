c4c6230f09f647716a8ef4a13b16421ae8c058e6
diegoferques <diegoferques@gmail.com>
2018-02-09 13:46:08 -0200

    Implementei o Controller e o Service indo no ViaCep - Corrigir erro de busca no VIACEP, pois se pesquisar o cep no navegador antes de buscar pelo resttemplate vai com sucesso, mas se for direto sem pesquisar antes no navegador vai dar erro

--- src/main/java/com/cosmeticos/controller/AddressController.java
+++ src/main/java/com/cosmeticos/controller/AddressController.java
@@ -1,7 +1,10 @@
 package com.cosmeticos.controller;
 
 import com.cosmeticos.commons.AddressResponseBody;
+import com.cosmeticos.model.Address;
+import com.cosmeticos.service.AddressService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 /**
  * Created by matto on 08/02/2018.
  */
@@ -17,7 +22,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AddressController {
 
-    @RequestMapping(path = "/address", method = RequestMethod.GET)
+    @Autowired
+    private AddressService addressService;
+
+    @RequestMapping(path = "/addresses", method = RequestMethod.GET)
     public HttpEntity<AddressResponseBody> findCepByWebService(@RequestParam("cep") String cep) {
 
         AddressResponseBody responseBody = new AddressResponseBody();
@@ -30,6 +38,41 @@ public class AddressController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseBody);
 
         } else {
+
+            try {
+
+                Optional<Address> addressOptional = addressService.findCepByWebService(cep);
+
+                if(addressOptional.isPresent()) {
+
+                    Address address = addressOptional.get();
+
+                    responseBody.setAddress(address);
+
+                    log.info("Busca de Address no WebService pelo cep com exito: [{}]", address);
+
+                    return ResponseEntity.status(HttpStatus.OK).body(responseBody);
+
+                } else {
+
+                    responseBody.setDescription("Nenhum Address encontrado com o CEP informado");
+
+                    log.error("Nenhum Address encontrado com o CEP informado: {}", cep);
+
+                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseBody);
+                }
+
+            } catch (Exception e) {
+
+                String errorCode = String.valueOf(System.nanoTime());
+
+                responseBody.setDescription("Houve um erro interno no servidor: " + errorCode);
+
+                log.error("Erro na exibição do Address solicitado com o cep: {} - {} - {}",
+                        cep, errorCode, e.getMessage(), e);
+
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseBody);
+            }
 
         }
 
